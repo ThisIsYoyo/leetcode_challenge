7896b556453e3c46c25b3451f10fe9b719e935f5
YoyoHsieh21 <yoyo.hsieh@gofreight.co>
2023-01-11 17:42:32 +0800

    refactor(test-problem): make test-case parametrize

--- test_problem.py
+++ test_problem.py
@@ -4,22 +4,24 @@ from typing import Tuple
 import pytest
 
 
-@pytest.mark.parametrize("file_str", [('p100')])
-def test_param_problem(file_str):
-    sys.path.append(f'problem/{file_str}')
-    import solution
-    import test_case
+SPECIFIC_PROBLEM_DIR_NAME = 'p1443'
 
-    for t_case in test_case.TEST_CASES:
-        t_input, t_output = t_case
+sys.path.append(f'problem/{SPECIFIC_PROBLEM_DIR_NAME}')
+import solution
+import test_case
 
-        sol = solution.Solution()
-        first_func_str = [func for func in dir(sol) if callable(getattr(sol, func)) and not func.startswith('__')][0]
-        sol_func = getattr(sol, first_func_str)
 
-        if isinstance(t_input, Tuple):
-            result = sol_func(*t_input)
-        else:
-            result = sol_func(t_input)
+@pytest.mark.parametrize("test_case", test_case.TEST_CASES)
+def test_problem(test_case):
+    t_input, t_output = test_case
 
-        assert result == t_output
+    sol = solution.Solution()
+    last_func_str = [func for func in dir(sol) if callable(getattr(sol, func)) and not func.startswith('__')][-1]
+    sol_func = getattr(sol, last_func_str)
+
+    if isinstance(t_input, Tuple):
+        result = sol_func(*t_input)
+    else:
+        result = sol_func(t_input)
+
+    assert result == t_output
