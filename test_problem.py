290a7035bb7d63ff71df412fe391086a233e6aa4
YoyoHsieh21 <yoyo.hsieh@gofreight.co>
2023-01-17 02:28:30 +0800

    fix(test-problem): extract wrong func on solution
    why
    - may extract other func since dir

--- test_problem.py
+++ test_problem.py
@@ -4,7 +4,7 @@ from typing import Tuple
 import pytest
 
 
-SPECIFIC_PROBLEM_DIR_NAME = 'p1443'
+SPECIFIC_PROBLEM_DIR_NAME = 'p57'
 
 sys.path.append(f'problem/{SPECIFIC_PROBLEM_DIR_NAME}')
 import solution
@@ -16,8 +16,8 @@ def test_problem(test_case):
     t_input, t_output = test_case
 
     sol = solution.Solution()
-    last_func_str = [func for func in dir(sol) if callable(getattr(sol, func)) and not func.startswith('__')][-1]
-    sol_func = getattr(sol, last_func_str)
+    no_underscore_func_str = [func for func in dir(sol) if callable(getattr(sol, func)) and '_' not in func][0]
+    sol_func = getattr(sol, no_underscore_func_str)
 
     if isinstance(t_input, Tuple):
         result = sol_func(*t_input)
