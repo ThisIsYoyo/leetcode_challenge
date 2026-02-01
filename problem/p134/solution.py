f9af2f1116893408788ef6b28f67befb2a0e37ae
ThisIsYoyo <b850108cd@gmail.com>
2026-02-01 22:01:22 +0800

    refactor(medium): 134

--- problem/p134/solution.py
+++ problem/p134/solution.py
@@ -1,16 +1,10 @@
 import math
-from typing import List
-
-
-class BuildInFunc:
-    pass
 
 
 class Solution:
-    def __init__(self, build_in_func: BuildInFunc):
-        self.build_in_func = build_in_func
+    process_function_str = "canCompleteCircuit"
 
-    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+    def canCompleteCircuit(self, gas: list[int], cost: list[int]) -> int:
         if sum(cost) > sum(gas):
             return -1
 
@@ -34,22 +28,3 @@ class Solution:
                 return -1
 
         return lowest_idx
-
-
-def get_test_cases() -> List:  # [(input1, output1), (input2, output2), ...]
-    return [
-        (([1, 2, 3, 4, 5], [3, 4, 5, 1, 2]), 3),
-        (([2, 3, 4], [3, 4, 3]), -1),
-    ]
-
-
-if __name__ == '__main__':
-    test_cases = get_test_cases()
-
-    for t_input, t_output in test_cases:
-        built_in_func = BuildInFunc()
-
-        sol = Solution(built_in_func)
-        result = sol.canCompleteCircuit(*t_input)
-
-        print(f'Is can complete expect ? `{result == t_output}`')
